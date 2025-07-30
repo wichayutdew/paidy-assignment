2bb2049e0ff9709ebb43be2bb4581b1e4ed74f28
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2025-07-30 20:15:06 +0700

    feat: support Empty and unknown currencies error

--- forex-mtl/project/Dependencies.scala
+++ forex-mtl/project/Dependencies.scala
@@ -9,6 +9,7 @@ object Dependencies {
     val http4s     = "0.22.15"
     val circe      = "0.14.2"
     val pureConfig = "0.17.4"
+    val enumeratum = "1.9.0"
 
     val kindProjector  = "0.13.2"
     val logback        = "1.2.3"
@@ -33,6 +34,7 @@ object Dependencies {
     lazy val circeGenericExt = circe("circe-generic-extras")
     lazy val circeParser     = circe("circe-parser")
     lazy val pureConfig      = "com.github.pureconfig" %% "pureconfig" % Versions.pureConfig
+    lazy val enumeratum      = "com.beachape"          %% "enumeratum" % Versions.enumeratum
 
     // Compiler plugins
     lazy val kindProjector = "org.typelevel" %% "kind-projector" % Versions.kindProjector cross CrossVersion.full
