3219f7674ade67d57a3cef26b39fb0569a7c22d2
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2025-07-31 20:36:04 +0700

    mocked application config

--- forex-mtl/src/test/scala/forex/config/ConfigSpec.scala
+++ forex-mtl/src/test/scala/forex/config/ConfigSpec.scala
@@ -1,13 +1,11 @@
 package forex.config
 import cats.effect.IO
-import forex.config.models.ApplicationConfig
 import forex.helper.MockedObject
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 import java.io.File
 import java.nio.file.Files
-import scala.concurrent.duration.{ FiniteDuration, SECONDS }
 
 class ConfigSpec extends AnyWordSpec with Matchers with MockedObject {
   "Config" when {
@@ -19,16 +17,7 @@ class ConfigSpec extends AnyWordSpec with Matchers with MockedObject {
         val result = Config.stream[IO]("app").compile.toList.unsafeRunSync()
 
         result.size shouldBe 1
-        val config: ApplicationConfig = result.head
-        config.server.host shouldBe "localhost"
-        config.server.port shouldBe 8080
-        config.server.requestTimeout shouldBe FiniteDuration(40, SECONDS)
-
-        config.client.oneFrame.host shouldBe "localhost"
-        config.client.oneFrame.port shouldBe 8090
-        config.client.oneFrame.requestTimeout shouldBe FiniteDuration(30, SECONDS)
-        config.client.oneFrame.connectionTimeout shouldBe FiniteDuration(30, SECONDS)
-        config.client.oneFrame.token shouldBe "test-token"
+        result.head shouldBe mockedApplicationConfig
 
         configFile.delete()
       }
--- forex-mtl/src/test/scala/forex/helper/MockedObject.scala
+++ forex-mtl/src/test/scala/forex/helper/MockedObject.scala
@@ -1,21 +1,23 @@
 package forex.helper
+import forex.config.models.{ ApplicationConfig, ClientSetting, OneFrameConfig, ServerSetting }
 import forex.domain.oneframe.RateDTO
 import forex.domain.rates._
 
 import java.time.{ OffsetDateTime, ZoneOffset }
+import scala.concurrent.duration.{ FiniteDuration, SECONDS }
 
 trait MockedObject {
 
   val mockedConfig: String = """app {
                                                 |  server {
                                                 |    host = "localhost"
-                                                |    port = 8080
+                                                |    port = 8090
                                                 |    request-timeout = 40 seconds
                                                 |  }
                                                 |  client {
                                                 |    one-frame {
                                                 |      host = "localhost"
-                                                |      port = 8090
+                                                |      port = 8080
                                                 |      request-timeout = 30 seconds
                                                 |      connection-timeout = 30 seconds
                                                 |      token = "test-token"
@@ -38,5 +40,22 @@ trait MockedObject {
     pair = Pair(Currency.CHF, Currency.CAD),
     price = Price(BigDecimal(1.2)),
     timestamp = mockedTimestamp
+  )
+
+  val mockedApplicationConfig: ApplicationConfig = ApplicationConfig(
+    server = ServerSetting(
+      host = "localhost",
+      port = 8090,
+      requestTimeout = FiniteDuration(40, SECONDS)
+    ),
+    client = ClientSetting(oneFrame =
+      OneFrameConfig(
+        host = "localhost",
+        port = 8080,
+        requestTimeout = FiniteDuration(30, SECONDS),
+        connectionTimeout = FiniteDuration(30, SECONDS),
+        token = "test-token"
+      )
+    )
   )
 }
