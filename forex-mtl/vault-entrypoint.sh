5f1af7bd4781332675b2cd82f14046ecfbacbf92
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2025-08-01 11:59:32 +0700

    feat: set own VAULT_TOKEN

--- forex-mtl/vault-entrypoint.sh
+++ forex-mtl/vault-entrypoint.sh
@@ -7,8 +7,6 @@ until vault status > /dev/null 2>&1; do
   sleep 1
 done
 
-export VAULT_ADDR='http://127.0.0.1:8200'
-export VAULT_TOKEN='root'
 vault kv put secret/forex/one-frame token="${ONE_FRAME_TOKEN}"
 
 wait $VAULT_PID
