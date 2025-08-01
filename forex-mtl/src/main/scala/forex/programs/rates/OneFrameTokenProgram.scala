8886a2ab2ee4dc1314192db0027bcb8a034061db
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2025-08-01 23:31:35 +0700

    feat: split program to reduce responsibility for token

---
+++ forex-mtl/src/main/scala/forex/programs/rates/OneFrameTokenProgram.scala
@@ -0,0 +1,31 @@
+package forex.programs.rates
+import cats.Monad
+import cats.data.EitherT
+import cats.implicits.toFlatMapOps
+import forex.config.models.CacheConfig
+import forex.domain.vault.Constant.{ Key, Path }
+import forex.programs.rates.errors.{ toProgramError, Error }
+import forex.services.SecretManagerService
+import forex.services.cache.interpreters.InMemoryCacheService
+
+class OneFrameTokenProgram[F[_]: Monad](
+    secretManagerService: SecretManagerService[F],
+    inMemoryCacheService: InMemoryCacheService[F],
+    tokenCacheConfig: CacheConfig
+) {
+  private val cacheKey = s"${tokenCacheConfig.prefix}_${Path.ONE_FRAME}"
+
+  def getToken: F[Error Either String] = if (tokenCacheConfig.enabled) {
+    inMemoryCacheService.get(cacheKey).flatMap {
+      case Some(token) => Monad[F].pure(Right(token))
+      case None        => fetchAndFillCache
+    }
+  } else fetchAndFillCache
+
+  private def fetchAndFillCache: F[Either[Error, String]] = (for {
+    token <- EitherT(secretManagerService.get(Path.ONE_FRAME, Key.TOKEN)).leftMap(toProgramError)
+  } yield {
+    if (tokenCacheConfig.enabled) { inMemoryCacheService.set(cacheKey, token, tokenCacheConfig.ttl) }
+    token
+  }).value
+}
